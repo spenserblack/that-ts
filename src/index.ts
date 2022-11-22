1eea1df6050eaa539591e57e004b49ba88ff8a7f
Spenser Black <spenserblack01@gmail.com>
2022-11-22 11:49:17 -0500

    Make types stricter
    Although loss is that more generic type arguments are required.

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,15 @@ export const prop =
   (item: T) =>
     item[prop];
 
-// TODO: Enforce more at compile-time, less at runtime
-export const fn = <T, P extends keyof T = keyof T, V extends T[P] = T[P]>(
-  fnName: P,
-  ...args: V extends (...args: any[]) => any ? Parameters<V> : never
+export type CallableProperty<P extends string | number | symbol, Args extends any[]> = {
+  [property in P]: (...args: Args) => unknown;
+};
+
+export const fn = <T extends CallableProperty<P, Args>, P extends keyof T, Args extends any[]>(
+  fn: P,
+  ...args: Parameters<T[P]>
 ) => {
   return (item: T) => {
-    const fn = item[fnName];
-    if (!(typeof fn === "function")) {
-      throw new TypeError(`'${String(fnName)}' must be a callable`);
-    }
-    return fn.call(item, ...args);
+    return item[fn].call(item, ...args);
   };
 };
