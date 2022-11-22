9b7ca346266b43b791e3462b13895f87e50d63e1
Spenser Black <spenserblack01@gmail.com>
2022-11-22 09:26:20 -0500

    Define `fn`

---
+++ __tests__/fn.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "@jest/globals";
+import { fn } from "../src/index";
+
+test("It returns a callable calls the named method", () => {
+  type Obj = {
+    add(a: number, b: number): number;
+  };
+  const obj: Obj = {
+    add: (a: number, b: number) => a + b,
+  };
+  const callable = fn<Obj>('add', 1, 2);
+  expect(callable(obj)).toBe(3);
+});
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,17 @@ export const prop =
   <T>(prop: keyof T) =>
   (item: T) =>
     item[prop];
+
+// TODO: Enforce more at compile-time, less at runtime
+export const fn = <T, P extends keyof T=keyof T, V extends T[P]=T[P]>(
+  fnName: P,
+  ...args: V extends (...args: any[]) => any ? Parameters<V> : never
+) => {
+  return (item: T) => {
+    const fn = item[fnName];
+    if (!(typeof fn === "function")) {
+      throw new TypeError(`'${String(fnName)}' must be a callable`);
+    }
+    return fn(...args);
+  };
+};
